97c5e426331980180f69f717114bf9d1ee3abc89
EJMarkle <emarkle99@gmail.com>
2024-03-19 18:22:59 -0500

    amenities

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Routes /status and /stats on object 'app_views'"""
-
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views
@@ -11,10 +10,12 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route("/status")
 def status_page():
     """Page declaring 'status': OK"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route("/stats")
 def stats_page():
