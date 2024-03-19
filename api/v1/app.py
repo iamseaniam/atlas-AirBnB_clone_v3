860a440165d9a813231612d1f009db9ad80e9d5a
EJMarkle <emarkle99@gmail.com>
2024-03-19 17:09:30 -0500

    stats - not working

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-""" Starts API """
-from api.v1.views import app_views
+"""Starts API"""
+
 from flask import Flask, jsonify
-from models import storage
 from os import environ
 from flask_cors import CORS
-
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
-
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """ Closes current SQLAlchemy session """
+    """Closes current SQLAlchemy session"""
     storage.close()
-
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "Not found"}), 404
-
 
 if __name__ == "__main__":
     host = '0.0.0.0'
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
-""" Routes /status on object 'app_views' and returns JSON status """
-from api.v1.views import app_views
+"""Routes /status and /stats on object 'app_views'"""
+
 from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-
-@app_views.route('/status', methods=['GET'])
-def get_api_status():
+@app_views.route("/status")
+def status_page():
+    """Page declaring 'status': OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats_page():
+    """Page relaying counts of each type of class objects"""
+    cls_to_plural = {"amenities": Amenity, "cities": City, "places": Place,
+                     "reviews": Review, "states": State, "users": User}
+    cls_count_dict = {key: storage.count(cls) for key,
+                      cls in cls_to_plural.items()}
+    return jsonify(cls_count_dict)
