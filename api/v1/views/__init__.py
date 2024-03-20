a24f0d07433b97788512eaa5c272f8f225c98f1b
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-03-20 14:27:58 -0500

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""Blueprint initialization"""
-from api.v1.views.users import *
-from api.v1.views.places_amenities import *
-from api.v1.views.amenities import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.index import *
+""" Creates app_views template """
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views import states, cities, amenities, users, places, reviews
